316790df3e05ebe3b596cb676437de784f69b014
PavanChitluri <pavankumar9990@gmail.com>
2018-09-09 18:18:57 -0400

    Update with description.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ *  Write a program that outputs the string representation of numbers from 1 to n.
+     But for multiples of three it should output “Fizz” instead of the number and for the 
+     multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+ */
+
 public class FizzBuzz {
 	public static void main(String[] args) {
 		FizzBuzz obj = new FizzBuzz();
 		List<String> list = obj.printString(30);
 		System.out.println(list);
 	}
-	
+
 	public List<String> printString(int n) {
 		List<String> list = new ArrayList<>();
 		if (n <= 0) {
@@ -18,18 +24,18 @@ public class FizzBuzz {
 			}
 
 		}
-		
-		for( int i=1 ; i<=n; i++) {
+
+		for (int i = 1; i <= n; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				list.add("FizzBuzz");
 			} else if (i % 3 == 0) {
 				list.add("Fizz");
-			} else if (i % 3 == 0) {
+			} else if (i % 5 == 0) {
 				list.add("Buzz");
 			} else {
 				list.add(new Integer(i).toString());
 			}
-			
+
 		}
 		return list;
 	}
--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,16 +1,29 @@
 import java.util.Arrays;
 
+/*
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than n/2  times.
+
+	You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+ */
 public class MajorityElement {
 	public static void main(String[] args) {
-		
+
 	}
-	
+
 	public int getElement(int[] nums) {
 		Arrays.sort(nums);
-		return nums[nums.length/2];
+		return nums[nums.length / 2];
 	}
-	
-	
+
 	public int majorityElement(int[] nums) {
 		int majorElement = nums[0];
 		int count = 1;
